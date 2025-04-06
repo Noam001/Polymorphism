6118fac79c411789de393c91aea4131011e2d856
Noam <Noam@COMP-NOAM>
2025-04-06 20:59:09 +0300

    Add project files.

---
+++ Polymorphism/BasicShape.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    internal class BasicShape
+    {
+        int borderColor;
+        Point point;
+        int fillColor;
+        public BasicShape(int borderColor, Point point, int fillColor)
+        {
+            this.borderColor = borderColor;
+            this.point = point;
+            this.fillColor = fillColor;
+        }
+
+    }
+}
